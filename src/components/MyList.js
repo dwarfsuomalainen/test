6130666725132693d5dbdeca4d352832e88ab74d
dwarfsuomalainen <r.bogoudinov@mac.com>
2022-07-23 15:15:48 +0300

    task 3

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,5 +1,18 @@
 function List(){
-    return <h2>Here is a list</h2>;
+    const header = "This is a header of List";
+    const items = [
+    {id: "1", text: "This is first item of list"},
+    {id: "2", text: "This is second item of list"}
+    ];
+    return (
+        <div>
+            <h1>{header}</h1>
+            <ol>
+                {items.map((item) => <li key={item.id}>{item.text}</li>)}
+            </ol>
+            
+        </div>
+    )
 }
 
-export default List;
+export default List
