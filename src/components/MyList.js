1d42b9c4c24c4cfdc2adbebae28de90b44337b44
dwarfsuomalainen <r.bogoudinov@mac.com>
2022-07-23 15:28:54 +0300

    task 3

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,17 +1,17 @@
-function List(){
+function List(props){
     const header = "This is a header of List";
     const items = [
     {id: "1", text: "This is first item of list"},
     {id: "2", text: "This is second item of list"}
     ];
     return (
-        <div>
+        <>
             <h1>{header}</h1>
             <ol>
-                {items.map((item) => <li key={item.id}>{item.text}</li>)}
+                {items.map((props) => <li key={props.id}>{props.text}</li>)}
             </ol>
             
-        </div>
+        </>
     )
 }
 
