534745c22cec2b1c11fd7d9254cc68de9b237fd7
dwarfsuomalainen <r.bogoudinov@mac.com>
2022-07-26 19:16:19 +0300

    task5

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -6,7 +6,7 @@ function MyList(props){
             <h1>{props.header}</h1>
             <ol>
                 {props.items.map((item) => 
-                <li key={item.id} onClick={() => props.updateItem(item.id)} style={{textDecoration: item.clicked ? 'line-through' : 'none'}}>{item.text} </li>)}
+                <li key={item.id} onClick={() => props.updateItem(item.id)} style={{textDecoration: item.clicked ? 'line-through' : ''}}>{item.text} </li>)}
 
             </ol>
             
